b3f4ed0189fb9d2ebad6e5d485ee959e287e4f22
haderalim <hader_alim_299@yahoo.com>
2018-06-15 03:44:21 +0200

    Update Gaussian_using_class.cpp

--- C- Tutorial/Object Oriented Programming/Gaussian_using_class.cpp
+++ C- Tutorial/Object Oriented Programming/Gaussian_using_class.cpp
@@ -88,5 +88,21 @@ Gaussian Gaussian::add(Gaussian other) {
 	return Gaussian(new_mu, new_sigma2);
 }
 
+int main ()
+{
 
+	Gaussian mygaussian(30.0,100.0);
+	Gaussian othergaussian(10.0,25.0);
+	
+	std::cout << "average " << mygaussian.getMu() << std::endl;
+	std::cout << "evaluation " << mygaussian.evaluate(15.0) << std::endl;
+
+	std::cout << "mul results variance " << mygaussian.mul(othergaussian).getSigma2() << std::endl;
+	std::cout << "mul results average " << mygaussian.mul(othergaussian).getMu() << std::endl;
+
+	std::cout << "add results variance " << mygaussian.add(othergaussian).getSigma2() << std::endl;
+	std::cout << "add results average " << mygaussian.add(othergaussian).getMu() << std::endl;
+
+	return 0;
+}
     
